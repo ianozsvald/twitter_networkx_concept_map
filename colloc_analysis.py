2736b3fb2c96eaa950c1a32216c25c8a9467aa5b
Ian Ozsvald <ian@ianozsvald.com>
2013-06-01 13:13:10 +0100

    futzing around

--- colloc_analysis.py
+++ colloc_analysis.py
@@ -3,6 +3,8 @@ import json
 import nltk
 from nltk.collocations import BigramCollocationFinder, TrigramCollocationFinder
 from nltk.corpus import stopwords
+#import extract_clean_tweet_content
+
 
 en_stopwords = set(w for w in stopwords.words('english'))
 
@@ -43,13 +45,29 @@ def extract_top_collocations(json_cleaned, return_top_n=10, use_trigrams=False):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Extract information from streaming tweet set')
     parser.add_argument('--json-cleaned', help='Cleaned input json')
+    parser.add_argument('--plain-text', help='File of plain text')
     args = parser.parse_args()
 
     items = []
-    for line in open(args.json_cleaned):
-        items.append(json.loads(line))
+    if args.json_cleaned:
+        for line in open(args.json_cleaned):
+            items.append(json.loads(line))
+    if args.plain_text:
+        for line in open(args.plain_text):
+            items.append({'tweet': line.strip()})
+
+    # the following is a hacky way of cleaning a tweet but the results do not
+    # markedly improve, so I'm commenting this out (maybe to revisit)
+    # force a cleaning of the tweet - ignore URL tweets, lowercase, clean some
+    # unicode
+    #for item in items:
+        #cleaned_set = extract_clean_tweet_content.clean_tweets([json.dumps(item)])
+        #cleaned_tweet = ""
+        #if len(cleaned_set):
+            #cleaned_tweet = cleaned_set.pop()
+        #item['tweet'] = cleaned_tweet
+
     top_collocations = extract_top_collocations(items)
     print top_collocations
     top_collocations = extract_top_collocations(items, use_trigrams=True)
     print top_collocations
-
